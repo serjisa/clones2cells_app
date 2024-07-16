5bf9591e29a8af4946f27ecce9b769dd1f31b336
Sergey Isaev <serj.hoop@gmail.com>
2024-07-16 14:06:06 +0200

    Update clones2cells_viewer.py

--- clones2cells_viewer.py
+++ clones2cells_viewer.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 css="""
 <style>
-    section.main > div {max-width:1600px}
+    section.main > div {max-width:1500px}
 </style>
 """
 st.markdown(css, unsafe_allow_html=True)
